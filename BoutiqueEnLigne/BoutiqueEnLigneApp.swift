544d0722ebca6771e05f8addb4490f7e3a58501a
mouss86 <cherifmoussa28@yahoo.fr>
2024-05-08 00:53:37 +0200

    Initial Commit

---
+++ BoutiqueEnLigne/BoutiqueEnLigneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoutiqueEnLigneApp.swift
+//  BoutiqueEnLigne
+//
+//  Created by Moussa Cherif on 2024-05-08.
+//
+
+import SwiftUI
+
+@main
+struct BoutiqueEnLigneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
